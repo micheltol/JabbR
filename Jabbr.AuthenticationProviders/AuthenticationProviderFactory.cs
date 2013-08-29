8caceba65fc46de61b8b99d5b91c24e8d03ff763
micheltol <micheltol@micheltol.nl>
2013-08-29 13:27:32 +0200

    Merged Jabbr/dev branch and updated the references

---
+++ Jabbr.AuthenticationProviders/AuthenticationProviderFactory.cs
@@ -0,0 +1,203 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using SimpleAuthentication.Core;
+//using SimpleAuthentication.Core.Config;
+//using SimpleAuthentication.Core.Tracing;
+
+//namespace Jabbr.AuthenticationProviders
+//{
+//    public class AuthenticationProviderFactory
+//    {
+//        private static readonly Lazy<IDictionary<string, IAuthenticationProvider>> Providers =
+//            new Lazy<IDictionary<string, IAuthenticationProvider>>(
+//                () =>
+//                {
+//                    var authenticationProviders = new Dictionary<string, IAuthenticationProvider>();
+//                    Initialize(authenticationProviders);
+//                    return authenticationProviders;
+//                });
+
+//        public AuthenticationProviderFactory()
+//        {
+//            TraceManager = new Lazy<ITraceManager>(() => new TraceManager()).Value;
+//        }
+
+//        public IDictionary<string, IAuthenticationProvider> AuthenticationProviders
+//        {
+//            get { return Providers.Value; }
+//        }
+
+//        public ITraceManager TraceManager { set; private get; }
+
+//        public void AddProvider(IAuthenticationProvider provider, bool replaceExisting = true)
+//        {
+//            AddProviderToDictionary(AuthenticationProviders, provider, replaceExisting);
+//        }
+
+//        public void RemoveProvider(string providerName)
+//        {
+//            RemoveProviderFromDictionary(providerName, AuthenticationProviders);
+//        }
+
+//        private static void Initialize(IDictionary<string, IAuthenticationProvider> authenticationProviders)
+//        {
+//            if (authenticationProviders == null)
+//            {
+//                throw new ArgumentNullException("authenticationProviders");
+//            }
+
+//            var discoveredProviders = MefHelpers.GetExportedTypes<IAuthenticationProvider>();
+//            if (discoveredProviders == null)
+//            {
+//                return;
+//            }
+
+//            // Try configure from custom config section.
+//            var providerConfig = ProviderConfigHelper.UseConfig();
+//            if (providerConfig != null)
+//            {
+//                SetupCustomConfigProviders(authenticationProviders, discoveredProviders, providerConfig);
+//            }
+//        }
+
+//        private static void SetupCustomConfigProviders(
+//            IDictionary<string, IAuthenticationProvider> authenticationProviders,
+//            IList<Type> discoveredProviders,
+//            ProviderConfiguration providerConfig)
+//        {
+//            if (authenticationProviders == null)
+//            {
+//                throw new ArgumentNullException("authenticationProviders");
+//            }
+
+//            if (discoveredProviders == null)
+//            {
+//                throw new ArgumentNullException("discoveredProviders");
+//            }
+
+//            if (providerConfig == null)
+//            {
+//                throw new ArgumentNullException("providerConfig");
+//            }
+
+//            if (providerConfig.Providers == null)
+//            {
+//                throw new ArgumentException("providerConfiguration.Providers");
+//            }
+
+//            foreach (ProviderKey provider in providerConfig.Providers)
+//            {
+//                var discoveredProvider = DiscoverProvider(discoveredProviders, provider);
+
+//                AddProviderToDictionary(authenticationProviders, discoveredProvider, false);
+//            }
+//        }
+
+//        private static IAuthenticationProvider DiscoverProvider(IEnumerable<Type> discoveredProviders,
+//                                                                ProviderKey providerKey)
+//        {
+//            if (discoveredProviders == null)
+//            {
+//                throw new ArgumentNullException("discoveredProviders");
+//            }
+
+//            if (providerKey == null)
+//            {
+//                throw new ArgumentNullException("providerKey");
+//            }
+
+//            var name = providerKey.Name.ToLowerInvariant();
+
+//            var provider = discoveredProviders.SingleOrDefault(x => x.Name.ToLowerInvariant().StartsWith(name));
+
+//            if (provider == null)
+//            {
+//                var errorMessage =
+//                    string.Format(
+//                        "Unable to find the provider [{0}]. Is there a provider dll available? Is there a typo in the provider name? Solution suggestions: Check to make sure the correct dll's are in the 'bin' directory and/or check the name to make sure there's no typo's in there. Example: If you're trying include the GitHub provider, make sure the name is 'github' (any case) and that the ExtraProviders dll exists in the 'bin' directory or make sure you've downloaded the package via NuGet -> install-package SimpleAuthentication.ExtraProviders.",
+//                        name);
+//                //TraceSource.TraceError(errorMessage);
+//                throw new ApplicationException(errorMessage);
+//            }
+
+//            IAuthenticationProvider authenticationProvider = null;
+
+//            // Make sure we have a provider with the correct constructor parameters.
+//            // How? If a person creates their own provider and doesn't offer a constructor
+//            // that has a sigle ProviderParams, then we're stuffed. So we need to help them.
+//            if (provider.GetConstructor(new[] { typeof(ProviderParams) }) != null)
+//            {
+//                var parameters = new ProviderParams
+//                {
+//                    PublicApiKey = providerKey.Key,
+//                    SecretApiKey = providerKey.Secret,
+//                    Scopes = string.IsNullOrEmpty(providerKey.Scope)
+//                                 ? null
+//                                 : providerKey.Scope.Split(new[] { ',' },
+//                                                           StringSplitOptions.RemoveEmptyEntries)
+//                };
+//                authenticationProvider = Activator.CreateInstance(provider, parameters) as IAuthenticationProvider;
+//            }
+
+//            if (authenticationProvider == null)
+//            {
+//                // We didn't find a proper constructor for the provider class we wish to instantiate.
+//                var errorMessage =
+//                    string.Format(
+//                        "The type {0} doesn't have the proper constructor. It requires a constructor that only accepts 1 argument of type ProviderParams. Eg. public MyProvider(ProviderParams providerParams){{ .. }}.",
+//                        provider.FullName);
+//                //TraceSource.TraceError(errorMessage);
+//                throw new ApplicationException(errorMessage);
+//            }
+
+//            return authenticationProvider;
+//        }
+
+//        private static void AddProviderToDictionary(
+//            IDictionary<string, IAuthenticationProvider> authenticationProviders,
+//            IAuthenticationProvider provider,
+//            bool replaceExisting = true)
+//        {
+//            if (provider == null)
+//            {
+//                throw new ArgumentNullException("provider");
+//            }
+
+//            if (authenticationProviders == null)
+//            {
+//                throw new ArgumentNullException("authenticationProviders");
+//            }
+
+//            var key = provider.Name.ToLower();
+
+//            if (authenticationProviders.ContainsKey(key) &&
+//                !replaceExisting)
+//            {
+//                throw new InvalidOperationException(
+//                    string.Format(
+//                        "The provider '{0}' already exists and cannot be overridden, either set `replaceExisting` to `true`, or remove the provider first.",
+//                        provider.Name));
+//            }
+
+//            authenticationProviders[key] = provider;
+//        }
+
+//        private static void RemoveProviderFromDictionary(string providerName,
+//                                                         IDictionary<string, IAuthenticationProvider>
+//                                                             authenticationProviders)
+//        {
+//            if (providerName == null)
+//            {
+//                throw new ArgumentNullException("providerName");
+//            }
+
+//            if (authenticationProviders == null)
+//            {
+//                throw new ArgumentNullException("authenticationProviders");
+//            }
+
+//            authenticationProviders.Remove(providerName);
+//        }
+//    }
+//}
---
+++ Jabbr.AuthenticationProviders/InMotivProvider.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+using SimpleAuthentication.Core.Exceptions;
+using SimpleAuthentication.Core.Providers.Google;
+using SimpleAuthentication.Core.Tracing;
+
+namespace SimpleAuthentication.Core.Providers
+{
+    // REFERENCE: https://developers.google.com/accounts/docs/OAuth2Login
+
+    public class InMotivProvider : BaseOAuth20Provider<AccessTokenResult>
+    {
+        private const string AccessTokenKey = "access_token";
+        private const string ExpiresInKey = "expires_in";
+        private const string TokenTypeKey = "token_type";
+
+        public InMotivProvider(ProviderParams providerParams)
+            : base("Google", providerParams)
+        {
+            AuthenticateRedirectionUrl = new Uri("https://accounts.google.com/o/oauth2/auth");
+        }
+
+        #region BaseOAuth20Token<AccessTokenResult> Implementation
+
+        protected override IRestResponse<AccessTokenResult> ExecuteRetrieveAccessToken(string authorizationCode,
+                                                                                       Uri redirectUri)
+        {
+            if (string.IsNullOrEmpty(authorizationCode))
+            {
+                throw new ArgumentNullException("authorizationCode");
+            }
+
+            if (redirectUri == null ||
+                string.IsNullOrEmpty(redirectUri.AbsoluteUri))
+            {
+                throw new ArgumentNullException("redirectUri");
+            }
+
+            var restRequest = new RestRequest("/o/oauth2/token", Method.POST);
+            restRequest.AddParameter("client_id", PublicApiKey);
+            restRequest.AddParameter("client_secret", SecretApiKey);
+            restRequest.AddParameter("redirect_uri", redirectUri.AbsoluteUri);
+            restRequest.AddParameter("code", authorizationCode);
+            restRequest.AddParameter("grant_type", "authorization_code");
+            //restRequest.AddParameter("hd", "inmotiv.net");
+
+
+            var restClient = RestClientFactory.CreateRestClient("https://accounts.google.com");
+            TraceSource.TraceVerbose("Retrieving Access Token endpoint: {0}",
+                                     restClient.BuildUri(restRequest).AbsoluteUri);
+
+            return restClient.Execute<AccessTokenResult>(restRequest);
+        }
+
+        protected new string CreateRedirectionQuerystringParameters(Uri callbackUri, string state)
+        {
+            if (callbackUri == (Uri) null)
+                throw new ArgumentNullException("callbackUri");
+            if (string.IsNullOrEmpty(state))
+                throw new ArgumentNullException("state");
+            return string.Format("client_id={0}&redirect_uri={1}&response_type=code{2}{3}&hd=inmotiv.net",
+                                 (object) this.PublicApiKey, (object) callbackUri.AbsoluteUri, (object) this.GetScope(),
+                                 (object) this.GetQuerystringState(state));
+        }
+
+        public override RedirectToAuthenticateSettings RedirectToAuthenticate(Uri callbackUri)
+        {
+            if (callbackUri == (Uri)null)
+                throw new ArgumentNullException("callbackUri");
+            if (this.AuthenticateRedirectionUrl == (Uri)null)
+                throw new AuthenticationException("AuthenticationRedirectUrl has no value. Please set the authentication Url location to redirect to.");
+            if (string.IsNullOrEmpty(this.PublicApiKey))
+                throw new AuthenticationException("PublicApiKey has no value. Please set this value.");
+            string state = Guid.NewGuid().ToString();
+            string uriString = string.Format("{0}?{1}", (object)this.AuthenticateRedirectionUrl.AbsoluteUri, (object)this.CreateRedirectionQuerystringParameters(callbackUri, state));
+            this.TraceSource.TraceInformation("Google redirection uri: {0}.", new object[1]
+      {
+        (object) uriString
+      });
+            return new RedirectToAuthenticateSettings()
+            {
+                RedirectUri = new Uri(uriString),
+                State = state
+            };
+        }
+
+        protected override AccessToken MapAccessTokenResultToAccessToken(AccessTokenResult accessTokenResult)
+        {
+            if (accessTokenResult == null)
+            {
+                throw new ArgumentNullException("accessTokenResult");
+            }
+
+            if (string.IsNullOrEmpty(accessTokenResult.AccessToken) ||
+                accessTokenResult.ExpiresIn <= 0 ||
+                string.IsNullOrEmpty(accessTokenResult.TokenType))
+            {
+                var errorMessage =
+                    string.Format(
+                        "Retrieved a Google Access Token but it doesn't contain one or more of either: {0}, {1} or {2}.",
+                        AccessTokenKey, ExpiresInKey, TokenTypeKey);
+                TraceSource.TraceError(errorMessage);
+                throw new AuthenticationException(errorMessage);
+            }
+
+            return new AccessToken
+            {
+                PublicToken = accessTokenResult.AccessToken,
+                ExpiresOn = DateTime.UtcNow.AddSeconds(accessTokenResult.ExpiresIn)
+            };
+        }
+
+        protected override UserInformation RetrieveUserInformation(AccessToken accessToken)
+        {
+            if (accessToken == null)
+            {
+                throw new ArgumentNullException("accessToken");
+            }
+
+            if (string.IsNullOrEmpty(accessToken.PublicToken))
+            {
+                throw new ArgumentException("accessToken.PublicToken");
+            }
+
+            IRestResponse<UserInfoResult> response;
+
+            try
+            {
+                var restRequest = new RestRequest("/oauth2/v2/userinfo", Method.GET);
+                restRequest.AddParameter(AccessTokenKey, accessToken.PublicToken);
+
+                var restClient = RestClientFactory.CreateRestClient("https://www.googleapis.com");
+
+                TraceSource.TraceVerbose("Retrieving user information. Google Endpoint: {0}",
+                                         restClient.BuildUri(restRequest).AbsoluteUri);
+
+                response = restClient.Execute<UserInfoResult>(restRequest);
+            }
+            catch (Exception exception)
+            {
+                var errorMessage =
+                    string.Format("Failed to retrieve any UserInfo data from the Google Api. Error Messages: {0}",
+                                  exception.RecursiveErrorMessages());
+                TraceSource.TraceError(errorMessage);
+                throw new AuthenticationException(errorMessage, exception);
+            }
+
+            if (response == null ||
+                response.StatusCode != HttpStatusCode.OK)
+            {
+                var errorMessage = string.Format(
+                    "Failed to obtain some UserInfo data from the Google Api OR the the response was not an HTTP Status 200 OK. Response Status: {0}. Response Description: {1}. Error Message: {2}.",
+                    response == null ? "-- null response --" : response.StatusCode.ToString(),
+                    response == null ? string.Empty : response.StatusDescription,
+                    response == null
+                        ? string.Empty
+                        : response.ErrorException == null
+                              ? "--no error exception--"
+                              : response.ErrorException.RecursiveErrorMessages());
+
+                TraceSource.TraceError(errorMessage);
+                throw new AuthenticationException(errorMessage);
+            }
+
+            // Lets check to make sure we have some bare minimum data.
+            if (string.IsNullOrEmpty(response.Data.Id))
+            {
+                const string errorMessage =
+                    "We were unable to retrieve the User Id from Google API, the user may have denied the authorization.";
+                TraceSource.TraceError(errorMessage);
+                throw new AuthenticationException(errorMessage);
+            }
+
+            return new UserInformation
+            {
+                Id = response.Data.Id,
+                Gender = string.IsNullOrEmpty(response.Data.Gender)
+                             ? GenderType.Unknown
+                             : GenderTypeHelpers.ToGenderType(response.Data.Gender),
+                Name = response.Data.Name,
+                Email = response.Data.Email,
+                Locale = response.Data.Locale,
+                Picture = response.Data.Picture,
+                UserName = response.Data.GivenName
+            };
+        }
+
+        #endregion
+
+        public override IEnumerable<string> DefaultScopes
+        {
+            get
+            {
+                return new[]
+                       {
+                           "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
+                       };
+            }
+        }
+    }
+}
