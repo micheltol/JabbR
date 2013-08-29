f661f5fa1da479c14633d065481612a50b4b165e
micheltol <micheltol@micheltol.nl>
2013-08-29 11:20:35 +0200

    updated with latest versions from Jabbr\Dev

--- JabbR/Services/IValidatorUserInformationService.cs
+++ JabbR/Services/IValidatorUserInformationService.cs
@@ -1,31 +1,30 @@
-﻿namespace JabbR.Services
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using SimpleAuthentication.Core;
+
+namespace JabbR.Services
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
-    using WorldDomination.Web.Authentication;
-
     public interface IValidatorUserInformationService
     {
         Boolean Validate(UserInformation userInformation);
-
     }
-
 
     public class ValidatorUserInformationService : IValidatorUserInformationService
     {
-        private readonly List<string> _validEmailSuffix; 
+        private readonly List<string> _validEmailSuffix;
 
         public ValidatorUserInformationService(IJabbrConfiguration configurationService)
         {
             _validEmailSuffix = new List<string>(configurationService.ValidEmailSuffix);
-
         }
 
         public bool Validate(UserInformation userInformation)
         {
-            return _validEmailSuffix.Any(validEmailSuffix => userInformation.Email.EndsWith(validEmailSuffix,StringComparison.InvariantCultureIgnoreCase));
+            return
+                _validEmailSuffix.Any(
+                    validEmailSuffix =>
+                    userInformation.Email.EndsWith(validEmailSuffix, StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
